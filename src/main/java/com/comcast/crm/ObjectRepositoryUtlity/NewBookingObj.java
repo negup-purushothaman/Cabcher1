61bd268c5bec677f0b8cabb941962ab131c8d050
negup-purushothaman <purushothaman@negup.com>
2025-12-24 14:50:20 +0530

    New changes

--- src/main/java/com/comcast/crm/ObjectRepositoryUtlity/NewBookingObj.java
+++ src/main/java/com/comcast/crm/ObjectRepositoryUtlity/NewBookingObj.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import Genericlabaries.JavaUtility;
+
 public class NewBookingObj {
 
 	WebDriver driver;
